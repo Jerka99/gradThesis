3f4cc60bef3ac9c63307a032e7cad70e8989e7fe
AntonioJerkovic <antoniojerka@gmail.com>
2024-10-06 13:36:14 +0200

    deployment purpose

--- frontend/lib/constants.dart
+++ frontend/lib/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
     static const String directionsUrl = "https://api.openrouteservice.org/v2/directions/driving-car/geojson";
-    // static const String backendUrl = "https://gradthesis.onrender.com";
-    static const String backendUrl = "http://localhost:8080";
+    static const String backendUrl = "https://gradthesis.onrender.com";
+    // static const String backendUrl = "http://localhost:8080";
     static const String mobileAccess = "http://10.0.2.2:8080";
 }
