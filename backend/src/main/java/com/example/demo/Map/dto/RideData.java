bd091aff8732fcd5abe772dbefe8b38fdbb57901
AntonioJerkovic <antoniojerka@gmail.com>
2024-10-06 20:37:19 +0200

    deployment purpose

--- backend/src/main/java/com/example/demo/Map/dto/RideData.java
+++ backend/src/main/java/com/example/demo/Map/dto/RideData.java
@@ -17,4 +17,12 @@ public class RideData {
     private Integer selectedMarkerIndex1;
     private Integer selectedMarkerIndex2;
     private Long createdBy;
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
 }
